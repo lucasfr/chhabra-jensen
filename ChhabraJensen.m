c76441ec3d6e198c4633845a17ec923fbd88f647
lucasfr <lucas.franca.14@ucl.ac.uk>
2016-09-26 18:43:57 +0100

    Tyding it up and comments improvement

--- ChhabraJensen.m
+++ ChhabraJensen.m
@@ -1,142 +1,144 @@
-%% THIS FUNCTION WAS DEVELOPED IN ORDER TO ESTIMATE THE MULTIFRACTAL SPECTRA 
-%% FROM TIME SERIES AND WAS DEVOLEP BASED ON A CODE ORIGINALLY WRITTEN BY
-%% J. G. V. MIRANDA.
+%%% THIS FUNCTION WAS DEVELOPED IN ORDER TO ESTIMATE THE MULTIFRACTAL SPECTRA 
+%%% FROM TIME SERIES AND WAS DEVOLEP BASED ON A CODE ORIGINALLY WRITTEN BY
+%%% J. G. V. MIRANDA.
 
 % L. G. S. FRANCA - lucas.franca.14@ucl.ac.uk
 
-%% THE VARIABLES OF THE FUNCTION CHJ CORRESPOND TO:
+%%% THE VARIABLES OF THE FUNCTION CHJ CORRESPOND TO:
 
-% % qi - INITIAL VALUE OF q
-% % qf - FINAL VALUE OF q
-% % dq - INCREMENT TO MADE ON THE q VALUE AT EACH ITERATION
-% % Rq AND Ra - MINIMUM R2 VALUE FOR q AND alpha, RESPECTIVELY
-% % Io - INITIAL SCALE (DYADIC)
-% % x AND y - x AND Y COORDINATES OF THE INPUT TIME SERIES
+%%% qInitial - INITIAL VALUE OF q
+%%% qFinal - FINAL VALUE OF q
+%%% qVar - INCREMENT TO MADE ON THE q VALUE AT EACH ITERATION
+%%% rDq AND rAlpha - MINIMUM R2 VALUE FOR q AND alpha, RESPECTIVELY
+%%% initialScale - INITIAL SCALE (DYADIC)
+%%% y - INPUT TIME SERIES
 
 
-%% ACKNOWLEDGEMENTS: I WOULD LIKE TO THANK J. G. V. MIRANDA, C. N. DE SANTANA 
-%% AND M. P. LEITE FOR THE COMMENTS AND SUPPORT ON THE DEVELOPMENT OF THIS
-%% CODE
+function [qDq,spectr,partFunc] = ChhabraJensen(y, qInitial, qFinal,...
+    qVar, scaleNum, rAlpha, rDq, initialScale)
 
-function [qDq,spectr,partFunc] = ChhabraJensen(x, y, qi, qf, dq, Np, Ra, Rq, Io)
+% ELEMENTS INDEX
+x = 1:length(y);
 
-%% CALCULATING THE NUMBER OF ELEMENTS
-N = length(x);
-
-%% NORMALISING THE VALUES OF THE VECTOR X
+% NORMALISING THE VALUES OF THE VECTOR X FOR THE PARTITION ESTIMATION
 x =x/x(end);
 
-%% ESTIMATING THE SUM OF ALL ELEMENTS IN THE TIME SERIES SEGMENT
-SumY = sum(y);
+% ESTIMATING THE SUM OF ALL ELEMENTS IN THE TIME SERIES SEGMENT
+sumY = sum(y);
 
-
-%% THIS VARIABLE INDICATES THE NUMBER OF SCALES WHICH WILL BE REMOVED 
-%% FROM THE PARTITION FUNCTION DURING THE CALCULUS
-
-% THIS IS MADE IN ORDER TO REMOVE CERTAIN SCALES THAT MIGHT NOT
-% FOLLOW THE SCALING PATTERN
-
-I=Io;
-
-%% LOOP OVER ALL THE Q VALUES, FROM THE INTIAL Q (qi) TO THE FINAL ONE (qf)
-%% WITH INCREASES OF (dq).
+% LOOP OVER ALL THE Q VALUES, FROM THE INTIAL Q (qInitial) TO THE FINAL 
+% ONE (qFinal) WITH INCREASES (qVar).
 
 % THIS LOOP IS INTENDED TO REALISE THE CALCULATIONS FOR ALL THE Q VALUES IN
 % ORDER TO DERIVE THE SPECTRA
 
-qix=((qf-qi)/dq)+1;     %NUMBER OF VALUES OF q
+qValues=((qFinal-qInitial)/qVar)+1;     % NUMBER OF VALUES OF q
 
-Mq = zeros(qix,Np+1);
-Md = zeros(qix,Np+1);
+Mq = zeros(qValues,scaleNum+1);
+Md = zeros(qValues,scaleNum+1);
 spectr = [];
 qDq = [];
 
-for q=qi:dq:qf
+for q=qInitial:qVar:qFinal
     
-    %% ALOCATING VARIABLES
-    Ma = zeros(Np+1,1);
-    Mf = zeros(Np+1,1);
-    mye = zeros(Np+1,1);
+    % ALOCATING VARIABLES
+    Ma = zeros(scaleNum+1,1);
+    Mf = zeros(scaleNum+1,1);
+    muScales = zeros(scaleNum+1,1);
     
     %% I - CALCULATING THE PARTITION FUNCTION
-    for k=I:Np
+    % FOR ALL THE SCALE SIZES
+    for k=initialScale:scaleNum
         
         Nor=0;
-        m=0;
         
-        Pr = 2^k;                 % MAXIMUM NUMBER OF POINTS (DYADIC SCALE)
-        E = 1/Pr;                 % SIZE OF EACH PARTITION
-        pos = k-I+1;
-        mye(pos,1) = log10(E);
+        window = 2^k;  % MAXIMUM NUMBER OF POINTS (DYADIC SCALE)
         
-        for i=1:Pr
+        E = 1/window;  % SIZE OF EACH PARTITION (X AXIS)- DIVIDING THE
+                       % SEGMENTS IN SMALLER PIECES. 
+                       
+        pos = k-initialScale+1; % TO ADAPT THE VECTOR TO MATLAB INDEXING
+                                % (STARTING ON 1)
+                                
+        muScales(pos,1) = log10(E); % LOG OF THE SEGMENT SIZE
+        
+        % AT EACH WINDOW ON THE TIME SERIES
+        for i=1:window
             
             % ESTIMATING m(epsilon,i) AND p(epsilon)
             
             m = calcSumM(x,y,(i-1)*E,i*E);
-            p = m/SumY;
+            p = m/sumY;
             
-            
-            % ESTIMATING THE GENERIC MEASURES FAMILY (mu(q,episilon))
+            % THIS IF STATEMENT AVOIDS ERRORS WHEN p AND q ARE EQUAL TO 0
             if(p~=0)
                 
-                Nor = Nor + p^q;
-                
-                %THIS IF STATEMENT AVOID ERRORS WHEN p AND q ARE EQUAL TO 0
+                Nor = Nor + p^q;     % THIS IS THE NORMALISATION FACTOR
                 
             end
             
         end
         
  %% II - CALCULATING THE MEASURES
-        for i=1:Pr % loop for scan over the segments
+        for i=1:window % loop for scan over the segments
             
-            p = calcSumM(x,y,(i-1)*E,i*E)/SumY;
+            p = calcSumM(x,y,(i-1)*E,i*E)/sumY;
             
             % IF TO AVOID DIVERGENCE DUE TO NULL MEASURES
             if(p~=0)
                 
-                if(q > (1-dq/2) && q < (1+dq/2))
+                % IF q IS BIGGER THAN 0 AND SMALLER THAN 1
+                % THIS IS MADE IN ORDER TO AVOID ISSUES WITH THE EXPONENT
+                if(q > (1-qVar/2) && q < (1+qVar/2))
                     
-                    Md(int64((q-qi)/dq)+1,k-I+1) = ...
-                        Md(int64((q-qi)/dq)+1,k-I+1) + ((p*log10(p))/Nor);
-                    
+                    Md(int64((q-qInitial)/qVar)+1,k-initialScale+1) = ...
+                        Md(int64((q-qInitial)/qVar)+1,k-initialScale+1)...
+                            + ((p*log10(p))/Nor);
+                
+                % FOR ANY OTHER VALUE OF q
                 else
                     
-                    Md(int64((q-qi)/dq)+1,k-I+1)= ...
-                    Md(int64((q-qi)/dq)+1,k-I+1) + p^q;
-
-                    
+                    Md(int64((q-qInitial)/qVar)+1,k-initialScale+1) = ...
+                    Md(int64((q-qInitial)/qVar)+1,k-initialScale+1) + p^q;
+     
                 end
                 
-                mu = (p^q)/Nor;
-                Ma(k-I+1,1) = Ma(k-I+1,1) + mu*log10(p);
-                Mf(k-I+1) = Mf(k-I+1) + mu*log10(mu);
+                mu = (p^q)/Nor;    % NORMALISED MEASURE
                 
+                Ma(k-initialScale+1,1) = Ma(k-initialScale+1,1)...
+                    + mu*log10(p); % MEASURE TO OBTAIN alpha
+                
+                Mf(k-initialScale+1) = Mf(k-initialScale+1) + mu*log10(mu);
+                                    % MEASURE TO OBTAIN f
+                                    
             end
             
             
         end
         
-        %% IF STATEMENT FOR q!=1
-        if(~(q > (1-dq/2) && q < (1+dq/2)))
+        % FOR q < 0 && q > 1 | LOG OF THE SUM
+        if(~(q > (1-qVar/2) && q < (1+qVar/2)))
             
-            Md(int64((q-qi)/dq)+1,k-I+1)= log10(Md(int64((q-qi)/dq)+1,k-I+1));
+            Md(int64((q-qInitial)/qVar)+1,k-initialScale+1)= log10(Md(int64((q-qInitial)/qVar)+1,k-initialScale+1));
             
         end
     end
     
     
     %% III - LINEAR REGRESSION AND EQUATION VARIABLES
-
-    [FAqb,~,FAqR2] = fitting(mye,Ma);
-    [FFqb,~,FFqR2] = fitting(mye,Mf);
-    [FDqb,~,FDqR2] = fitting(mye,Md((int64((q-qi)/dq)+1),:)');
     
-    %% f(alpha) SPECTRUM VARIABLES
+    % THIS PIECE ESTIMATES THE SPECTRAL VALUES FOR EACH VALUE OF q BASED ON
+    % THE REGRESSION OF THE VALUES OBTAINED FOR EACH SCALE
+    [FAqb,~,FAqR2] = fitting(muScales,Ma);
+    [FFqb,~,FFqR2] = fitting(muScales,Mf);
+    [FDqb,~,FDqR2] = fitting(muScales,Md((int64((q-qInitial)/qVar)+1),:)');
+    
+    
+    %% IV - ACCEPTANCE OR DENIAL OF VALUES
+    % f(alpha) SPECTRUM VARIABLES
     % IF STATEMENT TO CHECK WHETHER THE R2 FOR THE FIT REACH A MINIMUM
     % THRESHOLD OR NOT - alpha AND f(alpha)
-    if ( FAqR2 >= Ra && FFqR2 >= Ra || q==0)
+    if ( FAqR2 >= rAlpha && FFqR2 >= rAlpha || q==0)
         
         spectr(end+1,1) = q;              % q
         spectr(end,2) = FAqb;             % alpha
@@ -146,10 +148,9 @@ for q=qi:dq:qf
         
     end
     
-    %% Dq SPECTRUM VARIABLES
-    
+    % Dq SPECTRUM VARIABLES
     % IF STATEMENT FOR q=1
-    if((q>(1-dq/2) && q<(1+dq/2)))
+    if((q>(1-qVar/2) && q<(1+qVar/2)))
         
         Dq = FDqb;
         
@@ -161,7 +162,7 @@ for q=qi:dq:qf
     
     %% IF STATEMENT TO CHECK WHETHER THE R2 FOR THE FIT REACH A MINIMUM
     % THRESHOLD OR NOT - Dq
-    if ( FDqR2 >= Rq || q == 0)
+    if ( FDqR2 >= rDq || q == 0)
         
         qDq(end+1,1) = q;                      % q
         qDq(end,2) = Dq;                       % Dq
@@ -170,19 +171,16 @@ for q=qi:dq:qf
         
     end
     
-    %% PARTITION FUNCTION
-    partFunc = vertcat(mye',Md);
+    % PARTITION FUNCTION
+    partFunc = vertcat(muScales',Md);
     
 end
 end
 
-%% THIS ROUTINE WAS CREATED FOR THE ESTIMATION OF THE PARTITION FNCTIONS
-
-% IT WAS CONCEIVED FOR BEING USED WITH THE CHHABRA-JENSEN METHOD ALGORITHM,
-% ORIGINALLY
+%% SUMMATION FUNCTION
 
 % THE FUNCTION RECEIVES 4 INPUT VARIABLES, THE 'x' AND 'y' VARIABLES OF A
-% TIME SERIES, THE INITIAL POINT OF THE WINDOW 'EI' AND THE FINAL 'Ef'.
+% TIME SERIES, THE INITIAL POINT OF THE WINDOW 'Ei' AND THE FINAL 'Ef'.
 
 function [mysum] = calcSumM(x, y, Ei, Ef)
 
@@ -191,19 +189,20 @@ mysum = sum(y(x>Ei & x<=Ef));
 
 end
 
+%% FITTING FUNCTION
 function [b,a,R2] = fitting(x,y)
 
-%% CALCULATING THE COEFFICIENTS
+% CALCULATING THE COEFFICIENTS
 X = [ones(length(x),1) x];
 B = X\y;
 
 b = B(2);
 a = B(1);
 
-%% ESTIMATING TE LINE
+% ESTIMATING TE LINE
 yF = X*B;
 
-%% CALCULATING THE R2
+% CALCULATING THE R2
 R2 = 1 - sum((y - yF).^2)/sum((y - mean(y)).^2);
 
 end
