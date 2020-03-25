7695a72cfe21dba5833a571ae11ac6f17bc534c3
lucasfr <lucas.franca.14@ucl.ac.uk>
2020-03-25 00:52:54 +0000

    Python version

--- src/chhabrajensen.py
+++ src/chhabrajensen.py
@@ -2,7 +2,7 @@
  
 #  This software is licensed under an MIT License.
  
-#  Copyright (c) 2018 Lucas G S França, Yujiang Wang, José Miranda.
+#  Copyright (c) 2020 Lucas G S França, Yujiang Wang, José Miranda.
  
 #  Permission is hereby granted, free of charge, to any person obtaining a 
 #  copy of this software and associated documentation files (the "Software"), 
@@ -45,14 +45,13 @@
 #  J. G. V. MIRANDA, modified by L. G. S. FRANCA. It is based on the
 #  publication by Chhabra & Jensen 1989 PRL.
 
-#  This version is a faster and cleaner version of the previous codes
-#  written by Yujiang.Wang@newcastle.ac.uk 15th Dec 2016. It fully utilises
-#  the vectorisation in Matlab to speed up operations. A diagram summary of
-#  the algorithm can be found on the author's website www.ywang.co.uk
 
 #[alpha,falpha,Dq,Rsqr_alpha,Rsqr_falpha,Rsqr_Dq,muScale,Md,Ma,Mf]
 
-def ChhabraJensen_w0(Timeseries,qValues,scales)
+import numpy as np
+from scipy import stats
+
+def getSpectrum(Timeseries,qValues,scales):
 
 # INPUT:
 # Timeseries, qValues, and scales are all row vectors
@@ -69,102 +68,83 @@ def ChhabraJensen_w0(Timeseries,qValues,scales)
 #  falpha=falpha(kid);
 # muScale, Md, Ma, and Mf are the basic matricies from which alpha, falpha, and Dq can be constructed by linear regression (see code below line 81 - 92 for details). They are included in the output mainly for completeness.
 
-    import numpy as np
-    import scipy as scp
-
     ## initialise
-    nq = len(qValues)
-    ns = len(scales)
-    Ma = np.zeros(nq,ns)
-    Mf = np.zeros(nq,ns)
-    Md = np.zeros(nq,ns)
-    
-    muScale = -np.log10((2.^scales))
+    nq = qValues.shape[0]
+    ns = scales.shape[0]
+    Ma = np.zeros([nq,ns])
+    Mf = np.zeros([nq,ns])
+    Md = np.zeros([nq,ns])
+        
+    muScale = -np.log10(np.power(2,scales))
     #muScale=[muScale 0]
-    
-    alpha = np.zeros(nq,1)
-    falpha = np.zeros(nq,1)
-    Dq = np.zeros(nq,1)
-    
-    Rsqr_alpha = np.zeros(nq,1)
-    Rsqr_falpha = np.zeros(nq,1)
-    Rsqr_Dq = np.zeros(nq,1)
-    
-    
-    ## calculating Ma_ij Mf_ij Md_ij
-    for i in range(1,nq+1):
-        q = qValues(i-1)
+        
+    alpha = np.zeros([nq,1])
+    falpha = np.zeros([nq,1])
+    Dq = np.zeros([nq,1])
+        
+    Rsqr_alpha = np.zeros([nq,1])
+    Rsqr_falpha = np.zeros([nq,1])
+    Rsqr_Dq = np.zeros([nq,1])
 
-        for j in range(1,ns+1)
-            
+    ## calculating Ma_ij Mf_ij Md_ij
+    for i in range(nq):
+        
+        q = qValues[i]
+
+        for j in range(ns):
+                
             # determine how many windows we will have at this scale
-            window = 2^scales[j-1]
-            
+            window = 2**scales[j]
+                
             # break the time series into windows & sum
-            TimeseriesReshaped = reshape(Timeseries,[],window) <---------
+            TimeseriesReshaped = np.reshape(Timeseries, (-1, window), order="F")
             TimeseriesSummed = np.sum(Timeseries)
-            
-            %calculate p
-            ps = sum(TimeseriesReshaped,1) <-----------
+                
+            # calculate p
+            ps = np.sum(TimeseriesReshaped, axis = 0)
             p = np.divide(ps,TimeseriesSummed)
-            
-            
+                
+                
             Nor = sum(np.power(p,q))
-            
-            
+                
+                
             # calculation of Md
             Md[i,j] = np.log10(Nor); # not accounting for q between 0 and 1
-            if q<=1 && q>0 <-------
-                Md[i,j] = np.divide(sum(p*np.log10(p)),Nor)
             
-            
+            if q<=1 and q>0:
+                Md[i,j] = np.divide(sum(np.multiply(p,np.log10(p))),Nor)
+                
+                
             # Ma & Mf
             mu = np.divide((np.power(p,q)),Nor)
-            Ma(i,j)=sum(mu*np.log10(p))
-            Mf(i,j)=sum(mu*np.log10(mu))
+            Ma[i,j] = np.sum(np.multiply(mu,np.log10(p)))
+            Mf[i,j] = np.sum(np.multiply(mu,np.log10(mu)))
             
-        #looping through scales
-        
-        
-        %% regression part
-        [b_Ma,R2_Ma] = fitting(muScale,Ma(i,:))
-        [b_Mf,R2_Mf] = fitting(muScale,Mf(i,:))
-        [b_Md,R2_Md] = fitting(muScale,Md(i,:))
-        
+            #looping through scales
+            
+            
+        ## regression part
+        slope, intercept, r_value, p_value, std_err = stats.linregress(muScale,Ma[i,:])
+        b_Ma = slope
+        R2_Ma = r_value
+        slope, intercept, r_value, p_value, std_err = stats.linregress(muScale,Mf[i,:])
+        b_Mf = slope
+        R2_Mf = r_value
+        slope, intercept, r_value, p_value, std_err = stats.linregress(muScale,Md[i,:])
+        b_Md = slope
+        R2_Md = r_value
+            
         alpha[i] = b_Ma
         falpha[i] = b_Mf
-        Dq[i] = np.divide(b_Md,(q-1))
-        
-        if q<=1 && q>0 <----------
-                Dq[i]=b_Md
-        
-        
-        Rsqr_alpha(i)=R2_Ma
-        Rsqr_falpha(i)=R2_Mf
-        Rsqr_Dq(i)=R2_Md
-        
+        Dq[i] = np.divide(b_Md,(q - 1))
+            
+        if q<=1 and q>0: 
+            Dq[i] = b_Md
+            
+            
+        Rsqr_alpha[i] = R2_Ma
+        Rsqr_falpha[i] = R2_Mf
+        Rsqr_Dq[i] = R2_Md
+    return alpha,falpha,Dq,Rsqr_alpha,Rsqr_falpha,Rsqr_Dq,muScale,Md,Ma,Mf
+            
     #looping through qValues
-    
-    
-    
-# end of main function
-
-## fitting function
-#function [b,R2] = fitting(x,y)
-def fitting(x,y)
-
-y=y'
-% CALCULATING THE COEFFICIENTS
-X = [ones(length(x),1) x']
-B = X\y
-
-b = B(2)
-%a = B(1)
-
-% ESTIMATING THE LINE
-yF = X*B;
-
-% CALCULATING THE R2
-R2 = 1 - sum((y - yF).^2)/sum((y - mean(y)).^2)
-
-end
